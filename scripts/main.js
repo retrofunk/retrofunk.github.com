02accb1b85743404f79a60a79b5217c98b1b85b0
Miguel Mota <miguelmota2@gmail.com>
2011-10-14 02:03:38 -0700

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,24 @@
 $(window).load(function(){
+	
+	// Initialize nivoSlider
 	$('#slider').nivoSlider({
 		effect: 'fade',
 		directionNav: false,
 		controlNav: false
 	});
+	
+});
+
+$(document).ready(function(){
+	
+	// Open external links in new tab
+	$('a[href^=http]').live('click', function(){
+		window.open(this.href);
+		return false;
+	});
+	$('a[rel*=external]').live('click', function(){
+		window.open(this.href);
+		return false;
+	});
+	
 });
