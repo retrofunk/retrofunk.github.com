0fee07ee6e6c40ccf7586bbe05aa3a10fe8d2409
Miguel Mota <miguelmota2@gmail.com>
2011-10-18 13:45:29 -0700

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 	
 	
 	//$('.main-nav-ul a').live('click', function(){
-		$('.content').load('/about .content', function(){
+		$('.content').load('/about', function(){
 			alert('testing...');
 		});
 	//});
